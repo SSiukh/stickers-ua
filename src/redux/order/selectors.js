a8fb2765fec426d42bae406a6d241ab5f085c9ec
SSiukh <siukhsasha@gmail.com>
2025-03-31 01:50:44 +0300

    order location

--- src/redux/order/selectors.js
+++ src/redux/order/selectors.js
@@ -1,4 +1,4 @@
 export const selectContactData = (state) => state.order.contactData;
 export const selectOrderItems = (state) => state.order.items;
-export const selectDelivery = (state) => state.order.delivey;
+export const selectLocation = (state) => state.order.delivery.location;
 export const selecComment = (state) => state.order.comment;
